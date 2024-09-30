5339f2de64463b67ac892a41ade2f384886ecca7
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-09-30 19:52:27 +0500

    Chessboard HomeWork DONE

---
+++ Сhessboard/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chessboard
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите размер доски: ");
+			int boardSize = int.Parse(Console.ReadLine());
+
+			int cellSize = 5;
+			// Построение шахматной доски
+			for (int row = 0; row < boardSize; row++)
+			{
+				for (int cellRow = 0; cellRow < cellSize; cellRow++) // количество строк в одной клетке
+				{
+					for (int col = 0; col < boardSize; col++)
+					{
+						// Чередуем клетку в зависимости от позиции
+						if ((row + col) % 2 == 0)
+						{
+							for (int cellCol = 0; cellCol < cellSize; cellCol++) // количество столбцов в одной клетке
+							{
+								Console.Write("* ");
+							}
+						}
+						else
+						{
+							for (int cellCol = 0; cellCol < cellSize; cellCol++) // пустая клетка
+							{
+								Console.Write("  ");
+							}
+						}
+					}
+					Console.WriteLine();
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
