47379ea31626bb95e404c6dc87c4361d6ce43da7
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-10-04 04:10:58 +0500

    Snake Home Work DONE

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -13,34 +9,45 @@ namespace Snake
 			int x = 10, y = 10; // начальные координаты курсора
 			ConsoleKey key;
 
-			do
+			// Скрываем курсор
+			Console.CursorVisible = false;
+
+			try
 			{
-				Console.Clear();
-				Console.SetCursorPosition(x, y);
-				Console.Write("\u2588"); // "курсор" в виде заполненного квадратика из ASCII
-
-				key = Console.ReadKey(true).Key;
-
-				switch (key)
+				do
 				{
-					case ConsoleKey.W:
-					case ConsoleKey.UpArrow:
-						y = Math.Max(0, y - 1); // двигаем курсор вверх
-						break;
-					case ConsoleKey.S:
-					case ConsoleKey.DownArrow:
-						y = Math.Min(Console.WindowHeight - 1, y + 1); // вниз
-						break;
-					case ConsoleKey.A:
-					case ConsoleKey.LeftArrow:
-						x = Math.Max(0, x - 1); // влево
-						break;
-					case ConsoleKey.D:
-					case ConsoleKey.RightArrow:
-						x = Math.Min(Console.WindowWidth - 1, x + 1); // вправо
-						break;
-				}
-			} while (key != ConsoleKey.Escape); // программа завершится при нажатии Escape
+					Console.Clear();
+					Console.SetCursorPosition(x, y);
+					Console.Write("X"); // "курсор" в виде символа "X"
+
+					key = Console.ReadKey(true).Key;
+
+					switch (key)
+					{
+						case ConsoleKey.W:
+						case ConsoleKey.UpArrow:
+							y = Math.Max(0, y - 1); // двигаем курсор вверх
+							break;
+						case ConsoleKey.S:
+						case ConsoleKey.DownArrow:
+							y = Math.Min(Console.WindowHeight - 1, y + 1); // вниз
+							break;
+						case ConsoleKey.A:
+						case ConsoleKey.LeftArrow:
+							x = Math.Max(0, x - 1); // влево
+							break;
+						case ConsoleKey.D:
+						case ConsoleKey.RightArrow:
+							x = Math.Min(Console.WindowWidth - 1, x + 1); // вправо
+							break;
+					}
+				} while (key != ConsoleKey.Escape); // программа завершится при нажатии Escape
+			}
+			finally
+			{
+				// Возвращаем видимость курсора
+				Console.CursorVisible = true;
+			}
 		}
 	}
 }
